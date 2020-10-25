f50fb87566ec64642e671bda56e409e7ee1c8ca6
kamal <k0k03lc@homeoffice.wal-mart.com>
2020-10-26 00:03:17 +0530

    Getting Bean from application context

--- src/main/java/com/kamal/kishore/spring/ioc/SpringIocApplication.java
+++ src/main/java/com/kamal/kishore/spring/ioc/SpringIocApplication.java
@@ -1,5 +1,6 @@
 package main.java.com.kamal.kishore.spring.ioc;
 
+import main.java.com.kamal.kishore.spring.ioc.interfaces.ISimNetwork;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -7,6 +8,10 @@ public class SpringIocApplication {
     public static void main(String[] args) {
         System.out.println(("Application loading..."));
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-        System.out.println(("Application Loaded..."));
+        System.out.println(("..................Application Loaded............."));
+        ISimNetwork simNetwork = applicationContext.getBean("simNetwork", ISimNetwork.class);
+        simNetwork.makeCall();
+        simNetwork.useInternet();
+
     }
 }
--- src/main/java/com/kamal/kishore/spring/ioc/interfaces/impl/AirtelNetwork.java
+++ src/main/java/com/kamal/kishore/spring/ioc/interfaces/impl/AirtelNetwork.java
@@ -3,9 +3,9 @@ package main.java.com.kamal.kishore.spring.ioc.interfaces.impl;
 import main.java.com.kamal.kishore.spring.ioc.interfaces.ISimNetwork;
 
 public class AirtelNetwork implements ISimNetwork {
-    public AirtelNetwork(){
-        System.out.println("Airtel Network default constructor called");
-    }
+//    public AirtelNetwork(){
+//        System.out.println("Airtel Network default constructor called");
+//    }
     public void makeCall() {
         System.out.println("Airtel Network making call");
     }
