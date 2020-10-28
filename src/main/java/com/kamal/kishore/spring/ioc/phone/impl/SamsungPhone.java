de69d95d53f325f6f2cd9979e846c1d533cbc197
kamal <k0k03lc@homeoffice.wal-mart.com>
2020-10-28 22:40:32 +0530

    @Required annotation (not working)

--- src/main/java/com/kamal/kishore/spring/ioc/phone/impl/SamsungPhone.java
+++ src/main/java/com/kamal/kishore/spring/ioc/phone/impl/SamsungPhone.java
@@ -5,12 +5,14 @@ import main.java.com.kamal.kishore.spring.ioc.phone.ISmartPhone;
 import main.java.com.kamal.kishore.spring.ioc.sim.ISimNetwork;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Objects;
 
 @Getter
 public class SamsungPhone implements ISmartPhone {
+
 
     ISimNetwork simNetwork;
 
@@ -27,8 +29,8 @@ public class SamsungPhone implements ISmartPhone {
         return false;
     }
 
-    @Autowired
-    public void setSimNetwork(@Qualifier("airtelNetwork") ISimNetwork simNetwork) {
+    @Required
+    public void setSimNetwork(ISimNetwork simNetwork) {
         System.out.println("SETTER LOG");
         this.simNetwork = simNetwork;
     }
